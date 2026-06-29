02bb18e48c6e42c45e4ea72b1800d29fe0432e2f
Mattoizu <matias.patapia@gmail.com>
2026-06-29 02:13:51 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ const firebaseConfig = {
 
 const firebaseApp = initializeApp(firebaseConfig);
 const db = getFirestore(firebaseApp);
+const auth = getAuth(firebaseApp);
 const STATE_DOC = doc(db, "campana", "estado");
 
 let firestoreReady = false;
